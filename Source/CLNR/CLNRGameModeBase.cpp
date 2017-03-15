bdf70eb9dc43ee1fbc21b9e4b314f259b90990a1
Audun - Skole PC <audunking@gmail.com>
2017-03-15 10:25:40 +0100

    Fixed Conflicts

--- Source/CLNR/CLNRGameModeBase.cpp
+++ Source/CLNR/CLNRGameModeBase.cpp
@@ -10,12 +10,9 @@ void ACLNRGameModeBase::ChangePower(float Value)
 	if ((CurrentPower - Value) <= 0)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("No Power left!"));
-<<<<<<< HEAD
-		UGameplayStatics::OpenLevel(GetWorld(), FName("AudunMap"));
-		return;
-=======
 		UGameplayStatics::OpenLevel(GetWorld(), FName("CafeLevelTest"));
->>>>>>> origin/HansChristianBranch
+		return;
+			
 	}
 	CurrentPower -= Value;
 	UE_LOG(LogTemp, Warning, TEXT("Power is set to %f"), CurrentPower);
