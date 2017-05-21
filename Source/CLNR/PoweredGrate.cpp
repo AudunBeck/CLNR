fe45146d230e97baca1925723159e2c206fa8b0c
Desktop-Beck <audunking@gmail.com>
2017-05-21 14:46:41 +0200

    Rewritten PoweredGrate to be able to move positive or negative in one direction, XYZ.

--- Source/CLNR/PoweredGrate.cpp
+++ Source/CLNR/PoweredGrate.cpp
@@ -20,15 +20,73 @@ void APoweredGrate::PowerOn()
 void APoweredGrate::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	if (GetActorLocation() != FVector(0, 0, MovePoint)) //If the object is not where it should be, move it in that direction.
+	
+	if (GetActorLocation() != MovePoint) //If the object is not where it should be, move it in that direction.
 	{
 		FVector CurrentLocation = GetActorLocation();
-		CurrentLocation.Z -= DeltaTime*Speed;
-		if (CurrentLocation.Z <= MovePoint) //If it is about to pass the point it should move to, move it to the point it should move to.
+		if (MovePoint.X != 0)
 		{
-			CurrentLocation.Z = MovePoint;
+			if (MovePoint.X < 0)
+			{
+				CurrentLocation.X -= DeltaTime*Speed;
+				if (CurrentLocation.X <= MovePoint.X) //If it is about to pass the point it should move to, move it to the point it should move to.
+				{
+					CurrentLocation.X = MovePoint.X;
+				}
+			}
+			else if (MovePoint.X > 0)
+			{
+				CurrentLocation.X += DeltaTime*Speed;
+				if (CurrentLocation.X >= MovePoint.X) //If it is about to pass the point it should move to, move it to the point it should move to.
+				{
+					CurrentLocation.X = MovePoint.X;
+				}
+			}
 		}
+
+		else if (MovePoint.Y != 0)
+		{
+			if (MovePoint.Y < 0)
+			{
+				CurrentLocation.Y -= DeltaTime*Speed;
+				if (CurrentLocation.Y <= MovePoint.Y) //If it is about to pass the point it should move to, move it to the point it should move to.
+				{
+					CurrentLocation.Y = MovePoint.Y;
+				}
+			}
+
+			else if (MovePoint.Y > 0)
+			{
+				CurrentLocation.Y += DeltaTime*Speed;
+				if (CurrentLocation.Y >= MovePoint.Y) //If it is about to pass the point it should move to, move it to the point it should move to.
+				{
+					CurrentLocation.Y = MovePoint.Y;
+				}
+			}
+		}
+
+		else if (MovePoint.Z != 0)
+		{
+			if (MovePoint.Z < 0)
+			{
+				CurrentLocation.Z -= DeltaTime*Speed;
+				if (CurrentLocation.Z <= MovePoint.Z) //If it is about to pass the point it should move to, move it to the point it should move to.
+				{
+					CurrentLocation.Z = MovePoint.Z;
+				}
+			}
+
+			else if (MovePoint.Z > 0)
+			{
+				CurrentLocation.Z += DeltaTime*Speed;
+				if (CurrentLocation.Z >= MovePoint.Z) //If it is about to pass the point it should move to, move it to the point it should move to.
+				{
+					CurrentLocation.Z = MovePoint.Z;
+				}
+			}
+		}
+		
+		
 		SetActorLocation(CurrentLocation);
 
 	}
